bba37676f5e1ae34c61dccc43aa6b4335eb981dc
Michael Strobel <mstrobel97@gmail.com>
2023-07-17 09:09:34 +0200

    🐛 clean up and fixes

--- figures/Figure.py
+++ figures/Figure.py
@@ -1,4 +1,8 @@
+from typing import TYPE_CHECKING
 from Colour import Colour
+
+if TYPE_CHECKING:
+    from Chessboard import Chessboard
 
 
 class Figure:
@@ -7,3 +11,9 @@ class Figure:
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__} {str(self.colour)}"
+
+    def get_picture(self):
+        raise NotImplemented()
+
+    def get_moves(self, position: tuple[int, int], chessboard: "Chessboard") -> list[tuple[int, int]]:
+        raise NotImplemented()
