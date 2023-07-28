e4e9645a775c9d0605bbd25341c4fbac31020403
Eixix <tobiasbetz@hotmail.de>
2023-07-28 20:22:17 +0200

    Finished Checkmate

--- figures/Bishop.py
+++ figures/Bishop.py
@@ -1,4 +1,4 @@
-from typing import Optional, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
 from Colour import Colour
 from figures.Figure import Figure
@@ -19,8 +19,7 @@ class Bishop(Figure):
         return [*self._get_moves_top_right(position, chessboard), *self._get_moves_bottom_right(position, chessboard),
                 *self._get_moves_bottom_left(position, chessboard), *self._get_moves_top_left(position, chessboard)]
 
-    def _get_moves_top_right(self, position: tuple[int, int], chessboard: "Chessboard") -> Optional[
-        list[tuple[int, int]]]:
+    def _get_moves_top_right(self, position: tuple[int, int], chessboard: "Chessboard") -> list[tuple[int, int]] | None:
         x, y = position
         if x + 1 < 8 and y + 1 < 8:
             if chessboard.get_figure_from_position((x + 1, y + 1)) is None:
@@ -29,8 +28,8 @@ class Bishop(Figure):
                 return [(x + 1, y + 1)]
         return []
 
-    def _get_moves_bottom_right(self, position: tuple[int, int], chessboard: "Chessboard") -> Optional[
-        list[tuple[int, int]]]:
+    def _get_moves_bottom_right(self, position: tuple[int, int], chessboard: "Chessboard") -> list[tuple[
+        int, int]] | None:
         x, y = position
 
         if x + 1 < 8 and y - 1 >= 0:
@@ -40,8 +39,7 @@ class Bishop(Figure):
                 return [(x + 1, y - 1)]
         return []
 
-    def _get_moves_bottom_left(self, position: tuple[int, int], chessboard: "Chessboard") -> Optional[
-        list[tuple[int, int]]]:
+    def _get_moves_bottom_left(self, position: tuple[int, int], chessboard: "Chessboard") -> list[tuple[int, int]] | None:
         x, y = position
 
         if x - 1 >= 0 and y - 1 >= 0:
@@ -51,8 +49,7 @@ class Bishop(Figure):
                 return [(x - 1, y - 1)]
         return []
 
-    def _get_moves_top_left(self, position: tuple[int, int], chessboard: "Chessboard") -> Optional[
-        list[tuple[int, int]]]:
+    def _get_moves_top_left(self, position: tuple[int, int], chessboard: "Chessboard") -> list[tuple[int, int]] | None:
         x, y = position
 
         if x - 1 >= 0 and y + 1 < 8:
--- figures/Rook.py
+++ figures/Rook.py
@@ -1,4 +1,4 @@
-from typing import Optional, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
 from Colour import Colour
 from figures.Figure import Figure
@@ -19,7 +19,7 @@ class Rook(Figure):
         return [*self._get_moves_top(position, chessboard), *self._get_moves_bottom(position, chessboard),
                 *self._get_moves_left(position, chessboard), *self._get_moves_right(position, chessboard)]
 
-    def _get_moves_top(self, position: tuple[int, int], chessboard: "Chessboard") -> Optional[list[tuple[int, int]]]:
+    def _get_moves_top(self, position: tuple[int, int], chessboard: "Chessboard") -> list[tuple[int, int]] | None:
         x, y = position
         if x + 1 < 8:
             if chessboard.get_figure_from_position((x + 1, y)) is None:
@@ -28,7 +28,7 @@ class Rook(Figure):
                 return [(x + 1, y)]
         return []
 
-    def _get_moves_bottom(self, position: tuple[int, int], chessboard: "Chessboard") -> Optional[list[tuple[int, int]]]:
+    def _get_moves_bottom(self, position: tuple[int, int], chessboard: "Chessboard") -> list[tuple[int, int]] | None:
         x, y = position
 
         if x - 1 >= 0:
@@ -38,7 +38,7 @@ class Rook(Figure):
                 return [(x - 1, y)]
         return []
 
-    def _get_moves_left(self, position: tuple[int, int], chessboard: "Chessboard") -> Optional[list[tuple[int, int]]]:
+    def _get_moves_left(self, position: tuple[int, int], chessboard: "Chessboard") -> list[tuple[int, int]] | None:
         x, y = position
 
         if y - 1 >= 0:
@@ -48,7 +48,7 @@ class Rook(Figure):
                 return [(x, y - 1)]
         return []
 
-    def _get_moves_right(self, position: tuple[int, int], chessboard: "Chessboard") -> Optional[list[tuple[int, int]]]:
+    def _get_moves_right(self, position: tuple[int, int], chessboard: "Chessboard") -> list[tuple[int, int]] | None:
         x, y = position
 
         if y + 1 < 8:
